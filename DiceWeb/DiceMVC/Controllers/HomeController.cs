5bb8b5ec71f5a4f18ac245359800ec30e4b6ac75
RomanAdamczyk <RomanAdamczyk82@gmail.com>
2021-10-10 21:58:21 +0200

    dices

---
+++ DiceWeb/DiceMVC/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using DiceMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ShowDices(int count)
+        {
+            Game game = new Game();
+            Random generator = new Random();
+            game.Dices = new int[5,2];
+            for (int i=0; i<5; i++)
+            {
+                game.Dices[i, 0] = generator.Next(1, 7);
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DiceWeb/DiceMVC/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceMVC.Models
+{
+    public class Game
+    {
+        [DisplayName ("Wartość")]
+        public int[,] Dices { get; set; }
+        
+    }
+}
