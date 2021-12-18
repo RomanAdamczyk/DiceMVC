dc0371b0005a454049089daf2b2a4a8957323247
RomanAdamczyk <RomanAdamczyk82@gmail.com>
2021-12-18 13:43:04 +0100

    AddPlayer

--- DiceWeb/DiceMVC/Models/Player.cs
+++ DiceWeb/DiceMVC/Models/Player.cs
@@ -7,8 +7,11 @@ namespace DiceMVC.Models
 {
     public class Player
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public Dictionary<string, int> Values { get; set; }
-        public Dictionary<string, bool> FreeValues { get; set; }
+        public int Score { get; set; }
+
+        //public Dictionary<string, int> Values { get; set; }
+        //public Dictionary<string, bool> FreeValues { get; set; }
     }
 }
