9b4d0202d43fa3c8658bb49302a10d88f88e3073
RomanAdamczyk <RomanAdamczyk82@gmail.com>
2023-09-09 21:35:19 +0200

    improve game

--- DiceWeb/DiceMVC/Controllers/GameController.cs
+++ DiceWeb/DiceMVC/Controllers/GameController.cs
@@ -44,86 +44,8 @@ namespace DiceMVC.Controllers
         [HttpGet]
         public IActionResult GamePlay(int gameId)
         {
-            
-           // PlaygameVm model = new PlaygameVm() { GameId = gameId };
+
             _gameService.NextLap(gameId);
-            //var game = _gameService.GetGameById(gameId);
-            //model.CurrentPlayer = _gameService.GetCurrentPlayerValue(gameId, game.CurrentPlayerId);
-            //model.Players = _gameService.GetPlayersScores(gameId);
-            //model.Round = game.CurrentRound;
-            //model.Lap = game.CurrentLap;
-            //model.Dices = new List<DicesVm>();
-            //model.Dices.Add(new DicesVm());
-            //model.Dices[0].Dice1IsBlocked = false;
-            //model.Dices[0].Dice2IsBlocked = false;
-            //model.Dices[0].Dice3IsBlocked = false;
-            //model.Dices[0].Dice4IsBlocked = false;
-            //model.Dices[0].Dice5IsBlocked = false;
-            //for (int i=1;i<model.Lap;i++)
-            //{
-            //    model.Dices.Add(_gameService.GetDices(gameId, model.CurrentPlayer.PlayerId, model.Round, i));
-            //    model.Dices[i].Dice1ImgPath = "/Images/" + model.Dices[i].Dice1.ToString() + ".png";
-            //    model.Dices[i].Dice2ImgPath = "/Images/" + model.Dices[i].Dice2.ToString() + ".png";
-            //    model.Dices[i].Dice3ImgPath = "/Images/" + model.Dices[i].Dice3.ToString() + ".png";
-            //    model.Dices[i].Dice4ImgPath = "/Images/" + model.Dices[i].Dice4.ToString() + ".png";
-            //    model.Dices[i].Dice5ImgPath = "/Images/" + model.Dices[i].Dice5.ToString() + ".png";
-            //}
-            //model.Dices.Add( new DicesVm());
-
-            //model.Dices[model.Lap].GameId = gameId;
-            //model.Dices[model.Lap].Lap = model.Lap;
-            //model.Dices[model.Lap].Round = model.Round;
-            //model.Dices[model.Lap].PlayerId = model.CurrentPlayer.PlayerId;
-
-            //Random generator = new Random();
-
-            //if (model.Dices[model.Lap - 1].Dice1IsBlocked)
-            //{
-            //    model.Dices[model.Lap].Dice1 = model.Dices[model.Lap - 1].Dice1;
-            //} else
-            //{
-            //    model.Dices[model.Lap].Dice1 = generator.Next(1, 7);
-            //}
-            //model.Dices[model.Lap].Dice1ImgPath = "/Images/" + model.Dices[model.Lap].Dice1.ToString() + ".png";
-            //if (model.Dices[model.Lap - 1].Dice2IsBlocked)
-            //{
-            //    model.Dices[model.Lap].Dice2 = model.Dices[model.Lap - 1].Dice2;
-            //}
-            //else
-            //{
-            //    model.Dices[model.Lap].Dice2 = generator.Next(1, 7);
-            //}
-            //model.Dices[model.Lap].Dice2ImgPath = "/Images/" + model.Dices[model.Lap].Dice2.ToString() + ".png";
-            //if (model.Dices[model.Lap - 1].Dice3IsBlocked)
-            //{
-            //    model.Dices[model.Lap].Dice3 = model.Dices[model.Lap - 1].Dice3;
-            //}
-            //else
-            //{
-            //    model.Dices[model.Lap].Dice3 = generator.Next(1, 7);
-            //}
-            //model.Dices[model.Lap].Dice3ImgPath = "/Images/" + model.Dices[model.Lap].Dice3.ToString() + ".png";
-            //if (model.Dices[model.Lap - 1].Dice4IsBlocked)
-            //{
-            //    model.Dices[model.Lap].Dice4 = model.Dices[model.Lap - 1].Dice4;
-            //}
-            //else
-            //{
-            //    model.Dices[model.Lap].Dice4 = generator.Next(1, 7);
-            //}
-            //model.Dices[model.Lap].Dice4ImgPath = "/Images/" + model.Dices[model.Lap].Dice4.ToString() + ".png";
-            //if (model.Dices[model.Lap - 1].Dice5IsBlocked)
-            //{
-            //    model.Dices[model.Lap].Dice5 = model.Dices[model.Lap - 1].Dice5;
-            //}
-            //else
-            //{
-            //    model.Dices[model.Lap].Dice5 = generator.Next(1, 7);
-            //}
-            //model.Dices[model.Lap].Dice5ImgPath = "/Images/" + model.Dices[model.Lap].Dice5.ToString() + ".png";
-            //model.OptionalValues = new PlayerValueVM();
-            //model.OptionalValues =  _gameService.CountOptionalValues(model.Dices[model.Lap]);                    
-            //_gameService.GetDicesToSave(model.Dices[model.Lap]);
 
             return View(_gameService.TurnDices(gameId));
         }
@@ -168,49 +90,6 @@ namespace DiceMVC.Controllers
         public IActionResult EndGame(int gameId)
         {
             return RedirectToAction("Index", "Home");
-        }
-        [HttpGet]
-        public IActionResult ChooseValue (int gameId)
-        {
-            PlaygameVm model = new PlaygameVm() { GameId = gameId };
-            var game = _gameService.GetGameById(gameId);
-            model.CurrentPlayer = _gameService.GetCurrentPlayerValue(gameId, game.CurrentPlayerId);
-            model.Players = _gameService.GetPlayersScores(gameId);
-            model.Round = game.CurrentRound;
-            model.Lap = game.CurrentLap;
-            var dices = _gameService.GetDices(model.GameId, model.CurrentPlayer.PlayerId, model.Round, 3);
-            model.Dices = new List<DicesVm>();
-            model.Dices.Add(dices);
-            model.OptionalValues = new PlayerValueVM();
-            _gameService.CountOptionalValues(dices);
-            return View(model);
-        }
-        [HttpPost]
-        public IActionResult ChooseValue(PlaygameVm model)
-        {
-            UpdateValuesVm newValues = new UpdateValuesVm();
-            newValues.GameId = model.GameId;
-            newValues.PlayerId = model.CurrentPlayer.PlayerId;
-            newValues.ChooseValue = model.ChooseValue;
-            newValues.CurrentValues = new PlayerValueVM();
-            newValues.CurrentValues = model.CurrentPlayer;
-            newValues.OptionalValues = new PlayerValueVM();
-            newValues.OptionalValues = model.OptionalValues;
-            _gameService.UpdateValue(newValues);
-            var playerTurn = _gameService.GetPlayerTurn(model.GameId, model.CurrentPlayer.PlayerId);
-            if (playerTurn < model.PlayersCount-1) _gameService.NextPlayer(model.GameId, playerTurn);
-            else
-            {
-                if (model.Round < 13) _gameService.NextRound(model.GameId);
-                else
-                {//koniec gry
-                }
-            }
-            
-                 
-                return View();
-        }
-
-
+        }       
     }
 }
