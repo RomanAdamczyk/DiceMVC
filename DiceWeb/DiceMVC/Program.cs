0556f74f67a176b19e237daf8040d9af45aa8714
Roman Adamczyk <romanadamczyk82@gmail.com>
2024-03-06 11:23:16 +0100

    szkieletetowy bez startup

--- DiceWeb/DiceMVC/Program.cs
+++ DiceWeb/DiceMVC/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DiceMVC.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiceMVC
 {
@@ -20,7 +23,7 @@ namespace DiceMVC
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+ //                   webBuilder.UseStartup<Startup>();
                 });
     }
 }
